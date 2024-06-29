0df72b2fd5f80528638494aa9462feca67d0e6b3
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:17:26 +0630

    Format Code

--- State.Microservice/ModularService.cs
+++ State.Microservice/ModularService.cs
@@ -1,25 +1,22 @@
-﻿using State.Microservice.Features.State;
+﻿namespace State.Microservice;
 
-namespace State.Microservice
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient);
 
-            services.AddScoped<DA_State>();
-            services.AddScoped<BL_State>();
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
+        services.AddScoped<DA_State>();
+        services.AddScoped<BL_State>();
+        builder.Services.AddControllers().AddJsonOptions(opt =>
+        {
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-            return services;
-        }
+        return services;
     }
 }
