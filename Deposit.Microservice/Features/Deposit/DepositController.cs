21fac124d373f8540a730547c46cae193dc3aac6
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:14:36 +0630

    Format Code

--- Deposit.Microservice/Features/Deposit/DepositController.cs
+++ Deposit.Microservice/Features/Deposit/DepositController.cs
@@ -1,61 +1,59 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Deposit.Microservice.Features.Deposit
+namespace Deposit.Microservice.Features.Deposit;
+
+[Route("api/v1/deposit")]
+[ApiController]
+public class DepositController : BaseController
 {
-    [Route("api/v1/deposit")]
-    [ApiController]
-    public class DepositController : BaseController
+    #region Initializations
+
+    private readonly BL_Deposit _bL_Deposit;
+
+    public DepositController(BL_Deposit bL_Deposit)
     {
-        #region Initializations
-
-        private readonly BL_Deposit _bL_Deposit;
-
-        public DepositController(BL_Deposit bL_Deposit)
-        {
-            _bL_Deposit = bL_Deposit;
-        }
-
-        #endregion
-
-        #region GetDepositListByAccountNo
-
-        [HttpGet]
-        public async Task<IActionResult> GetDepositListByAccountNo(string accountNo)
-        {
-            try
-            {
-                var responseModel = await _bL_Deposit.GetDepositListByAccountNoAsync(accountNo);
-                return Content(responseModel);
-            }
-            catch (Exception ex)
-            {
-                return HandleFailure(ex);
-            }
-        }
-
-        #endregion
-
-        #region CreateDeposit
-
-        [HttpPost]
-        public async Task<IActionResult> CreateDeposit([FromBody] DepositRequestModel requestModel)
-        {
-            try
-            {
-                var result = requestModel.IsValid();
-                if (!result.Success)
-                    return BadRequest(result);
-
-                var responseModel = await _bL_Deposit.CreateDepositAsync(requestModel);
-                return Content(responseModel);
-            }
-            catch (Exception ex)
-            {
-                return HandleFailure(ex);
-            }
-        }
-
-        #endregion
+        _bL_Deposit = bL_Deposit;
     }
+
+    #endregion
+
+    #region GetDepositListByAccountNo
+
+    [HttpGet]
+    public async Task<IActionResult> GetDepositListByAccountNo(string accountNo)
+    {
+        try
+        {
+            var responseModel = await _bL_Deposit.GetDepositListByAccountNoAsync(accountNo);
+            return Content(responseModel);
+        }
+        catch (Exception ex)
+        {
+            return HandleFailure(ex);
+        }
+    }
+
+    #endregion
+
+    #region CreateDeposit
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDeposit([FromBody] DepositRequestModel requestModel)
+    {
+        try
+        {
+            var result = requestModel.IsValid();
+            if (!result.Success)
+                return BadRequest(result);
+
+            var responseModel = await _bL_Deposit.CreateDepositAsync(requestModel);
+            return Content(responseModel);
+        }
+        catch (Exception ex)
+        {
+            return HandleFailure(ex);
+        }
+    }
+
+    #endregion
 }
