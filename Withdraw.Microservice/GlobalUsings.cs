78aa6b5e19c8b87ecc6eaec626e275988d01e6b3
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:46:49 +0630

    Format Code

--- Withdraw.Microservice/GlobalUsings.cs
+++ Withdraw.Microservice/GlobalUsings.cs
@@ -2,8 +2,10 @@
 
 global using Microservices.DotNet8MiniBankingManagementSystem.DbService.AppDbContexts;
 global using Microservices.DotNet8MiniBankingManagementSystem.Mapper;
+global using Microservices.DotNet8MiniBankingManagementSystem.Models.Enums;
 global using Microservices.DotNet8MiniBankingManagementSystem.Models.Features;
 global using Microservices.DotNet8MiniBankingManagementSystem.Models.Features.Withdraw;
 global using Microservices.DotNet8MiniBankingManagementSystem.Models.Resources;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
+global using Newtonsoft.Json;
