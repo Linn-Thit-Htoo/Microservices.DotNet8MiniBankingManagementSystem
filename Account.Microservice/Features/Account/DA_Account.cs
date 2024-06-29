10da73b6542392aa305be05bb24e20f383fc337e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:10:26 +0630

    Format Code

--- Account.Microservice/Features/Account/DA_Account.cs
+++ Account.Microservice/Features/Account/DA_Account.cs
@@ -5,124 +5,122 @@ using Microservices.DotNet8MiniBankingManagementSystem.Models.Resources;
 using Microsoft.EntityFrameworkCore;
 using Microservices.DotNet8MiniBankingManagementSystem.Mapper;
 
-namespace Account.Microservice.Features.Account
+namespace Account.Microservice.Features.Account;
+
+public class DA_Account
 {
-    public class DA_Account
+    #region Initializations
+
+    private readonly AppDbContext _context;
+
+    public DA_Account(AppDbContext context)
     {
-        #region Initializations
-
-        private readonly AppDbContext _context;
-
-        public DA_Account(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        #endregion
-
-        #region Get Account List Async
-
-        public async Task<Result<AccountListResponseModel>> GetAccountListAsync()
-        {
-            Result<AccountListResponseModel> responseModel;
-            try
-            {
-                var lst = await _context
-                    .TblAccounts.AsNoTracking()
-                    .OrderByDescending(x => x.AccountId)
-                    .ToListAsync();
-
-                var accountLst = lst.Select(x => x.Change()).ToList();
-                var model = new AccountListResponseModel { DataLst = accountLst };
-
-                responseModel = Result<AccountListResponseModel>.SuccessResult(
-                    model,
-                    MessageResource.Success
-                );
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<AccountListResponseModel>.FailureResult(ex);
-            }
-
-            return responseModel;
-        }
-
-        #endregion
-
-        #region Create Account
-
-        public async Task<Result<AccountResponseModel>> CreateAccount(AccountRequestModel requestModel)
-        {
-            Result<AccountResponseModel> responseModel;
-            try
-            {
-                bool isStatePresent = await _context
-                    .TblStates.AsNoTracking()
-                    .AnyAsync(x => x.StateCode == requestModel.StateCode);
-                if (!isStatePresent)
-                    throw new Exception("State does not exist.");
-
-                bool isTownshipPresent = await _context
-                    .TblTownships.AsNoTracking()
-                    .AnyAsync(x => x.TownshipCode == requestModel.TownshipCode);
-                if (!isTownshipPresent)
-                    throw new Exception("Township does not exist.");
-
-                var townshipLst = await _context
-                    .TblTownships.AsNoTracking()
-                    .Where(x => x.StateCode == requestModel.StateCode)
-                    .ToListAsync();
-
-                bool isTownshipValid = townshipLst.Any(township =>
-                    township.TownshipCode == requestModel.TownshipCode
-                );
-                if (!isTownshipValid)
-                    throw new Exception("Township is invalid.");
-
-                requestModel.CustomerCode = await GenerateCustomerCodeAsync();
-                requestModel.AccountLevel = 1;
-                await _context.TblAccounts.AddAsync(requestModel.Change());
-                int result = await _context.SaveChangesAsync();
-
-                responseModel = Result<AccountResponseModel>.ExecuteResult(result);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<AccountResponseModel>.FailureResult(ex);
-            }
-
-            return responseModel;
-        }
-
-        #endregion
-
-        #region Generate Customer Code
-
-        private async Task<string> GenerateCustomerCodeAsync()
-        {
-            var latestCustomerCode = await _context
-                .TblAccounts.AsNoTracking()
-                .OrderByDescending(a => a.CustomerCode)
-                .Select(a => a.CustomerCode)
-                .FirstOrDefaultAsync();
-
-            string newCustomerCode;
-
-            if (string.IsNullOrEmpty(latestCustomerCode))
-            {
-                newCustomerCode = "C00001";
-            }
-            else
-            {
-                int numericPart = int.Parse(latestCustomerCode.Substring(1)) + 1;
-                newCustomerCode = $"C{numericPart:D5}";
-            }
-
-            return newCustomerCode;
-        }
-
-        #endregion
+        _context = context;
     }
 
+    #endregion
+
+    #region Get Account List Async
+
+    public async Task<Result<AccountListResponseModel>> GetAccountListAsync()
+    {
+        Result<AccountListResponseModel> responseModel;
+        try
+        {
+            var lst = await _context
+                .TblAccounts.AsNoTracking()
+                .OrderByDescending(x => x.AccountId)
+                .ToListAsync();
+
+            var accountLst = lst.Select(x => x.Change()).ToList();
+            var model = new AccountListResponseModel { DataLst = accountLst };
+
+            responseModel = Result<AccountListResponseModel>.SuccessResult(
+                model,
+                MessageResource.Success
+            );
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<AccountListResponseModel>.FailureResult(ex);
+        }
+
+        return responseModel;
+    }
+
+    #endregion
+
+    #region Create Account
+
+    public async Task<Result<AccountResponseModel>> CreateAccount(AccountRequestModel requestModel)
+    {
+        Result<AccountResponseModel> responseModel;
+        try
+        {
+            bool isStatePresent = await _context
+                .TblStates.AsNoTracking()
+                .AnyAsync(x => x.StateCode == requestModel.StateCode);
+            if (!isStatePresent)
+                throw new Exception("State does not exist.");
+
+            bool isTownshipPresent = await _context
+                .TblTownships.AsNoTracking()
+                .AnyAsync(x => x.TownshipCode == requestModel.TownshipCode);
+            if (!isTownshipPresent)
+                throw new Exception("Township does not exist.");
+
+            var townshipLst = await _context
+                .TblTownships.AsNoTracking()
+                .Where(x => x.StateCode == requestModel.StateCode)
+                .ToListAsync();
+
+            bool isTownshipValid = townshipLst.Any(township =>
+                township.TownshipCode == requestModel.TownshipCode
+            );
+            if (!isTownshipValid)
+                throw new Exception("Township is invalid.");
+
+            requestModel.CustomerCode = await GenerateCustomerCodeAsync();
+            requestModel.AccountLevel = 1;
+            await _context.TblAccounts.AddAsync(requestModel.Change());
+            int result = await _context.SaveChangesAsync();
+
+            responseModel = Result<AccountResponseModel>.ExecuteResult(result);
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<AccountResponseModel>.FailureResult(ex);
+        }
+
+        return responseModel;
+    }
+
+    #endregion
+
+    #region Generate Customer Code
+
+    private async Task<string> GenerateCustomerCodeAsync()
+    {
+        var latestCustomerCode = await _context
+            .TblAccounts.AsNoTracking()
+            .OrderByDescending(a => a.CustomerCode)
+            .Select(a => a.CustomerCode)
+            .FirstOrDefaultAsync();
+
+        string newCustomerCode;
+
+        if (string.IsNullOrEmpty(latestCustomerCode))
+        {
+            newCustomerCode = "C00001";
+        }
+        else
+        {
+            int numericPart = int.Parse(latestCustomerCode.Substring(1)) + 1;
+            newCustomerCode = $"C{numericPart:D5}";
+        }
+
+        return newCustomerCode;
+    }
+
+    #endregion
 }
