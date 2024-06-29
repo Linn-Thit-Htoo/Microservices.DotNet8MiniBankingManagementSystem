e0865e47b29e1c246d2cd776a2966dd6ef80bbc9
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:46:19 +0630

    Format Code

--- Withdraw.Microservice/Features/Withdraw/BL_Withdraw.cs
+++ Withdraw.Microservice/Features/Withdraw/BL_Withdraw.cs
@@ -1,41 +1,40 @@
-﻿namespace Withdraw.Microservice.Features.Withdraw
+﻿namespace Withdraw.Microservice.Features.Withdraw;
+
+public class BL_Withdraw
 {
-    public class BL_Withdraw
+    #region Initialization
+
+    private readonly DA_Withdraw _dA_Withdraw;
+
+    public BL_Withdraw(DA_Withdraw dAWithdraw)
     {
-        #region Initialization
-
-        private readonly DA_Withdraw _dA_Withdraw;
-
-        public BL_Withdraw(DA_Withdraw dAWithdraw)
-        {
-            _dA_Withdraw = dAWithdraw;
-        }
-
-        #endregion
-
-        #region Get With Draw List By Account No Async
-
-        public async Task<Result<WithdrawListResponseModel>> GetWithDrawListByAccountNoAsync(
-            string accountNo
-        )
-        {
-            if (string.IsNullOrWhiteSpace(accountNo))
-                throw new Exception("Account No cannot be empty.");
-
-            return await _dA_Withdraw.GetWithDrawListByAccountNoAsync(accountNo);
-        }
-
-        #endregion
-
-        #region Create With draw Async
-
-        public async Task<Result<WithdrawResponseModel>> CreateWithDrawAsync(
-            WithdrawRequestModel requestModel
-        )
-        {
-            return await _dA_Withdraw.CreateWithDrawAsync(requestModel);
-        }
-
-        #endregion
+        _dA_Withdraw = dAWithdraw;
     }
+
+    #endregion
+
+    #region Get With Draw List By Account No Async
+
+    public async Task<Result<WithdrawListResponseModel>> GetWithDrawListByAccountNoAsync(
+        string accountNo
+    )
+    {
+        if (string.IsNullOrWhiteSpace(accountNo))
+            throw new Exception("Account No cannot be empty.");
+
+        return await _dA_Withdraw.GetWithDrawListByAccountNoAsync(accountNo);
+    }
+
+    #endregion
+
+    #region Create With draw Async
+
+    public async Task<Result<WithdrawResponseModel>> CreateWithDrawAsync(
+        WithdrawRequestModel requestModel
+    )
+    {
+        return await _dA_Withdraw.CreateWithDrawAsync(requestModel);
+    }
+
+    #endregion
 }
