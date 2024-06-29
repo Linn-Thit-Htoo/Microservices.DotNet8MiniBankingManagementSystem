a34d729066a6ee014a6e91b28658e15278704159
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:18:56 +0630

    Format Code

--- Township.Microservice/Features/Township/BL_Township.cs
+++ Township.Microservice/Features/Township/BL_Township.cs
@@ -1,35 +1,33 @@
-﻿namespace Township.Microservice.Features.Township
+﻿namespace Township.Microservice.Features.Township;
+
+public class BL_Township
 {
-    public class BL_Township
+    #region Initializations
+
+    private readonly DA_Township _dA_Township;
+
+    public BL_Township(DA_Township dA_Township)
     {
-        #region Initializations
-
-        private readonly DA_Township _dA_Township;
-
-        public BL_Township(DA_Township dA_Township)
-        {
-            _dA_Township = dA_Township;
-        }
-
-        #endregion
-
-        #region Get Township List Async
-
-        public async Task<Result<TownshipListResponseModel>> GetTownshipListAsync()
-        {
-            return await _dA_Township.GetTownshipListAsync();
-        }
-
-        #endregion
-
-        #region Create Township List Async
-
-        public async Task<Result<TownshipResponseModel>> CreateTownshipListAsync()
-        {
-            return await _dA_Township.CreateTownshipListAsync();
-        }
-
-        #endregion
+        _dA_Township = dA_Township;
     }
 
+    #endregion
+
+    #region Get Township List Async
+
+    public async Task<Result<TownshipListResponseModel>> GetTownshipListAsync()
+    {
+        return await _dA_Township.GetTownshipListAsync();
+    }
+
+    #endregion
+
+    #region Create Township List Async
+
+    public async Task<Result<TownshipResponseModel>> CreateTownshipListAsync()
+    {
+        return await _dA_Township.CreateTownshipListAsync();
+    }
+
+    #endregion
 }
