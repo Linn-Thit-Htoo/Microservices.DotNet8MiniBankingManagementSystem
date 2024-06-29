f0b3919e7e2d62c3ee03e03a9d4cda3cdd3d8455
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:21:53 +0630

    Format Code

--- TransactionHistory.Microservice/Features/BaseController.cs
+++ TransactionHistory.Microservice/Features/BaseController.cs
@@ -1,36 +1,32 @@
-﻿using Microservices.DotNet8MiniBankingManagementSystem.Models.Enums;
-using Newtonsoft.Json;
+﻿namespace TransactionHistory.Microservice.Features;
 
-namespace TransactionHistory.Microservice.Features
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object? obj)
     {
-        protected IActionResult Content(object? obj)
-        {
-            return Ok(JsonConvert.SerializeObject(obj));
-        }
+        return Ok(JsonConvert.SerializeObject(obj));
+    }
 
-        protected IActionResult Accepted(EnumRespType enumRespType)
+    protected IActionResult Accepted(EnumRespType enumRespType)
+    {
+        switch (enumRespType)
         {
-            switch (enumRespType)
-            {
-                case EnumRespType.Created:
-                    return StatusCode(201, MessageResource.SaveSuccess);
-                case EnumRespType.Accepted:
-                    return StatusCode(202, MessageResource.SaveSuccess);
-                case EnumRespType.Deleted:
-                    return StatusCode(202, MessageResource.DeleteSuccess);
-                case EnumRespType.None:
-                default:
-                    return BadRequest("Invalid Response Type.");
-            }
+            case EnumRespType.Created:
+                return StatusCode(201, MessageResource.SaveSuccess);
+            case EnumRespType.Accepted:
+                return StatusCode(202, MessageResource.SaveSuccess);
+            case EnumRespType.Deleted:
+                return StatusCode(202, MessageResource.DeleteSuccess);
+            case EnumRespType.None:
+            default:
+                return BadRequest("Invalid Response Type.");
         }
+    }
 
-        protected IActionResult HandleFailure(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+    protected IActionResult HandleFailure(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
