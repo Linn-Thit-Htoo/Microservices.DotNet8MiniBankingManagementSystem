72b2bd98bfaeea88e3fd9895eedad8a25100a577
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:04:22 +0630

    Format Code

--- Microservices.DotNet8MiniBankingManagementSystem.Gateway/Program.cs
+++ Microservices.DotNet8MiniBankingManagementSystem.Gateway/Program.cs
@@ -3,10 +3,7 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,9 +12,8 @@ builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseOcelot().Wait(); // Add this line
+app.UseOcelot().Wait();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
