9b230ce7ff37d26ea6ac51737d2dfa778b5ea8b7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:16:37 +0630

    Format Code

--- State.Microservice/Features/State/BL_State.cs
+++ State.Microservice/Features/State/BL_State.cs
@@ -1,34 +1,33 @@
-﻿namespace State.Microservice.Features.State
+﻿namespace State.Microservice.Features.State;
+
+public class BL_State
 {
-    public class BL_State
+    #region Initializations
+
+    private readonly DA_State _dA_State;
+
+    public BL_State(DA_State dA_State)
     {
-        #region Initializations
-
-        private readonly DA_State _dA_State;
-
-        public BL_State(DA_State dA_State)
-        {
-            _dA_State = dA_State;
-        }
-
-        #endregion
-
-        #region Get State List Async
-
-        public async Task<Result<StateListResponseModel>> GetStateListAsync()
-        {
-            return await _dA_State.GetStateListAsync();
-        }
-
-        #endregion
-
-        #region Create States Async
-
-        public async Task<Result<StateResponseModel>> CreateStatesAsync()
-        {
-            return await _dA_State.CreateStatesAsync();
-        }
-
-        #endregion
+        _dA_State = dA_State;
     }
+
+    #endregion
+
+    #region Get State List Async
+
+    public async Task<Result<StateListResponseModel>> GetStateListAsync()
+    {
+        return await _dA_State.GetStateListAsync();
+    }
+
+    #endregion
+
+    #region Create States Async
+
+    public async Task<Result<StateResponseModel>> CreateStatesAsync()
+    {
+        return await _dA_State.CreateStatesAsync();
+    }
+
+    #endregion
 }
