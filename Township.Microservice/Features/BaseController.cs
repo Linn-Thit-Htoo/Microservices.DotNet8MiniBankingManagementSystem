42f7880a8116485eef83d75337d671d187ff6719
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:19:23 +0630

    Format Code

--- Township.Microservice/Features/BaseController.cs
+++ Township.Microservice/Features/BaseController.cs
@@ -1,7 +1,6 @@
 ﻿using Microservices.DotNet8MiniBankingManagementSystem.Models.Enums;
 using Microservices.DotNet8MiniBankingManagementSystem.Models.Resources;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Township.Microservice.Features
 {
--- Township.Microservice/Features/Township/TownshipController.cs
+++ Township.Microservice/Features/Township/TownshipController.cs
@@ -1,56 +1,52 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace Township.Microservice.Features.Township;
 
-namespace Township.Microservice.Features.Township
+[Route("api/v1/township")]
+[ApiController]
+public class TownshipController : BaseController
 {
-    [Route("api/v1/township")]
-    [ApiController]
-    public class TownshipController : BaseController
+    #region Initializations
+
+    private readonly BL_Township _bL_Township;
+
+    public TownshipController(BL_Township bL_Township)
     {
-        #region Initializations
-
-        private readonly BL_Township _bL_Township;
-
-        public TownshipController(BL_Township bL_Township)
-        {
-            _bL_Township = bL_Township;
-        }
-
-        #endregion
-
-        #region GetTownshipList
-
-        [HttpGet]
-        public async Task<IActionResult> GetTownshipList()
-        {
-            try
-            {
-                return Content(await _bL_Township.GetTownshipListAsync());
-            }
-            catch (Exception ex)
-            {
-                return HandleFailure(ex);
-            }
-        }
-
-        #endregion
-
-        #region CreateTownshipList
-
-        [HttpPost]
-        public async Task<IActionResult> CreateTownshipList()
-        {
-            try
-            {
-                var responseModel = await _bL_Township.CreateTownshipListAsync();
-                return Content(responseModel);
-            }
-            catch (Exception ex)
-            {
-                return HandleFailure(ex);
-            }
-        }
-
-        #endregion
+        _bL_Township = bL_Township;
     }
+
+    #endregion
+
+    #region GetTownshipList
+
+    [HttpGet]
+    public async Task<IActionResult> GetTownshipList()
+    {
+        try
+        {
+            return Content(await _bL_Township.GetTownshipListAsync());
+        }
+        catch (Exception ex)
+        {
+            return HandleFailure(ex);
+        }
+    }
+
+    #endregion
+
+    #region CreateTownshipList
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTownshipList()
+    {
+        try
+        {
+            var responseModel = await _bL_Township.CreateTownshipListAsync();
+            return Content(responseModel);
+        }
+        catch (Exception ex)
+        {
+            return HandleFailure(ex);
+        }
+    }
+
+    #endregion
 }
