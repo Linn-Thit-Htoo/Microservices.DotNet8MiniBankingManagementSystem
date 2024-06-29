7c9f1e7d7f30b6c4603428d09ebdb1f01042ff15
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-30 01:16:52 +0630

    Format Code

--- State.Microservice/Features/State/DA_State.cs
+++ State.Microservice/Features/State/DA_State.cs
@@ -1,69 +1,68 @@
-﻿namespace State.Microservice.Features.State
+﻿namespace State.Microservice.Features.State;
+
+public class DA_State
 {
-    public class DA_State
+    #region Initializations
+
+    private readonly AppDbContext _appDbContext;
+
+    public DA_State(AppDbContext appDbContext)
     {
-        #region Initializations
-
-        private readonly AppDbContext _appDbContext;
-
-        public DA_State(AppDbContext appDbContext)
-        {
-            _appDbContext = appDbContext;
-        }
-
-        #endregion
-
-        #region Get State List Async
-
-        public async Task<Result<StateListResponseModel>> GetStateListAsync()
-        {
-            Result<StateListResponseModel> responseModel;
-            try
-            {
-                var states = await _appDbContext
-                    .TblStates.AsNoTracking()
-                    .OrderByDescending(x => x.StateId)
-                    .ToListAsync();
-
-                var lst = states.Select(x => x.Change()).ToList();
-                var model = new StateListResponseModel { DataLst = lst };
-
-                responseModel = Result<StateListResponseModel>.SuccessResult(model);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<StateListResponseModel>.FailureResult(ex);
-            }
-
-            return responseModel;
-        }
-
-        #endregion
-
-        #region Create States Async
-
-        public async Task<Result<StateResponseModel>> CreateStatesAsync()
-        {
-            Result<StateResponseModel> responseModel;
-            try
-            {
-                string jsonStr = await File.ReadAllTextAsync("Data/StateList.json");
-                List<TblState> lst = JsonConvert.DeserializeObject<
-                    List<TblState>
-                >(jsonStr)!;
-                await _appDbContext.TblStates.AddRangeAsync(lst);
-                int result = await _appDbContext.SaveChangesAsync();
-
-                responseModel = Result<StateResponseModel>.ExecuteResult(result);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<StateResponseModel>.FailureResult(ex);
-            }
-
-            return responseModel;
-        }
-
-        #endregion
+        _appDbContext = appDbContext;
     }
+
+    #endregion
+
+    #region Get State List Async
+
+    public async Task<Result<StateListResponseModel>> GetStateListAsync()
+    {
+        Result<StateListResponseModel> responseModel;
+        try
+        {
+            var states = await _appDbContext
+                .TblStates.AsNoTracking()
+                .OrderByDescending(x => x.StateId)
+                .ToListAsync();
+
+            var lst = states.Select(x => x.Change()).ToList();
+            var model = new StateListResponseModel { DataLst = lst };
+
+            responseModel = Result<StateListResponseModel>.SuccessResult(model);
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<StateListResponseModel>.FailureResult(ex);
+        }
+
+        return responseModel;
+    }
+
+    #endregion
+
+    #region Create States Async
+
+    public async Task<Result<StateResponseModel>> CreateStatesAsync()
+    {
+        Result<StateResponseModel> responseModel;
+        try
+        {
+            string jsonStr = await File.ReadAllTextAsync("Data/StateList.json");
+            List<TblState> lst = JsonConvert.DeserializeObject<
+                List<TblState>
+            >(jsonStr)!;
+            await _appDbContext.TblStates.AddRangeAsync(lst);
+            int result = await _appDbContext.SaveChangesAsync();
+
+            responseModel = Result<StateResponseModel>.ExecuteResult(result);
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<StateResponseModel>.FailureResult(ex);
+        }
+
+        return responseModel;
+    }
+
+    #endregion
 }
